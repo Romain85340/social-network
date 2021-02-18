660bdde4b1c419933e696ddaaf345b5708fc3c5c
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2021-02-18 01:37:15 +0100

    CRUD User fait

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,3 +21,105 @@ module.exports.userInfo = (req, res) => {
         }).select("-password");
     }
 }
+
+module.exports.updateUser = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send("ID inconnu : " + req.params.id)
+    }
+    else {
+        try {
+            await UserModel.findOneAndUpdate(
+                {_id: req.params.id}, 
+                {
+                    $set: {
+                        bio: req.body.bio
+                    }
+                },
+                { new: true, upsert: true, setDefaultsOnInsert: true},
+                (err, docs) => {
+                    if(!err) return res.send(docs);
+                    if(err) return res.status(500).json({ message: err })
+                }
+            )
+        } catch(err){
+            return res.status(500).json({ message: err })
+        }
+    }
+}
+
+module.exports.deleteUser = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send("ID inconnu : " + req.params.id)
+    } else {
+        try {
+            await UserModel.remove({ _id: req.params.id }).exec()
+            res.status(200).json({ message: "Utilisateur supprimé "})
+        } catch(err){
+            return res.status(500).json({ message: err })
+        }
+    }
+}
+
+module.exports.follow = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow)){
+        return res.status(400).send("ID inconnu : " + req.params.id)
+    }
+    else {
+        try {
+            // add to the follower list
+            await UserModel.findByIdAndUpdate(
+                { _id: req.params.id },
+                {$addToSet: { following: req.body.idToFollow }}, 
+                {new: true, upsert: true},
+                (err, docs) => {
+                    if(!err) res.status(201).json(docs)
+                    else return res.status(400).json(err)
+                }
+            );
+            // add to the following list
+            await UserModel.findByIdAndUpdate(
+                req.body.idToFollow,
+                { $addToSet: {followers: req.params.id}},
+                {new: true, upsert: true},
+                (err, docs) => {
+                    // if(!err) res.status(201).json(docs)
+                    if(err) return res.status(400).json(err)
+                }
+            )
+        } catch(err){
+            return res.status(500).json({ message: err })
+        }
+    }
+}
+
+module.exports.unfollow = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow)){
+        return res.status(400).send("ID inconnu : " + req.params.id)
+    }
+    else {
+        try {
+            // remove to the follower list
+            await UserModel.findByIdAndUpdate(
+                { _id: req.params.id },
+                {$pull: { following: req.body.idToUnfollow }}, 
+                {new: true, upsert: true},
+                (err, docs) => {
+                    if(!err) res.status(201).json(docs)
+                    else return res.status(400).json(err)
+                }
+            );
+            // remove to the following list
+            await UserModel.findByIdAndUpdate(
+                req.body.idToUnfollow,
+                { $pull: {followers: req.params.id}},
+                {new: true, upsert: true},
+                (err, docs) => {
+                    // if(!err) res.status(201).json(docs)
+                    if(err) return res.status(400).json(err)
+                }
+            )
+        } catch(err){
+            return res.status(500).json({ message: err })
+        }
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,11 @@ const userController = require("../controllers/user.controller");
 router.post("/register", authController.signUp);
 
 // user display: "block",
-router.get('/', userController.getAllUsers)
-router.get("/:id", userController.userInfo)
+router.get('/', userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
 
 module.exports = router;
