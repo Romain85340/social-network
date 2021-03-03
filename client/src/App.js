250b5b11c41801210ae478f90ff7c752a5774e68
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2021-03-03 09:41:36 +0100

    page user fait

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Routes from './components/Routes'
 import { UidContext } from './components/App.Context'
 import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { getUser } from './actions/user.actions';
 
 const App = () => {
   const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -20,6 +23,8 @@ const App = () => {
       .catch((err) => console.log("no token"))
     }
     fetchToken();
+
+    if(uid) dispatch(getUser(uid))
   }, [uid])
 
   return (
